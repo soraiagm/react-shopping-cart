c912e19c39111cfbb53b954065858ca57df84550
Soraia <soraia@Soraias-iMac.local>
2019-12-10 15:42:39 -0500

    completed delete stretch and localStorage stretch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
+import { useLocalStorage } from './hooks/useLocalStorage.js';
 import data from './data';
 
 import { ProductContext } from '../src/contexts/ProductContext.js';
@@ -12,17 +13,29 @@ import ShoppingCart from './components/ShoppingCart';
 
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	// const [cart, setCart] = useState([]);
+	// const [count, setCount] = useLocalStorage("count", []);
+	const [cart, setCart] = useLocalStorage("count", []);
+
+	// useEffect(() => {
+	// }, [count])
 
 	const addItem = item => {
 		// add the given item to the cart
+		// setCount([...cart, item])
 		setCart([...cart, item])
 	};
+
+	const deleteItem = (id) => {
+		console.log("id", id);
+		const itemDeleted = cart.filter(item => item.id !== id);
+		setCart([...itemDeleted])
+	}
 
 	return (
 		<div className="App">
  		  <ProductContext.Provider value={ {products, addItem} }>
-		   <CartContext.Provider value={ { cart } }>
+		   <CartContext.Provider value={ { cart, deleteItem } }>
 			 <Navigation component={Navigation} />
 			 <Route exact path="/" component={Products} />
 			 <Route path="/cart" component={ShoppingCart} />
